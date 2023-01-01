2c3fd380779457033043107d3e3becb604bb3043
slavivo <slavivo4@fit.cvut.cz>
2023-01-01 16:56:10 +0100

    Refinements

--- lineqs.py
+++ lineqs.py
@@ -5,7 +5,7 @@ import numpy as np
 import argparse
 
 # Function that solves a system of linear equations using the matrix method with Jacobi's method
-def jacobi(A, b, tol, maxiter):
+def jacobi(A, b, tol, maxiter, ref):
     k = 0
     D = np.diag(np.diag(A))
     L = np.tril(A) - D
@@ -14,16 +14,21 @@ def jacobi(A, b, tol, maxiter):
     error = np.linalg.norm(np.dot(A, x) - b).astype(np.float64)
     flag = 0
 
-    # Check if spectral radius of E - D^-1 * A >= 1 to guarantee non-convergence
-    if np.max(np.abs(np.linalg.eigvals(np.eye(20) - np.dot(np.linalg.inv(D), A)))) >= 1:
-        flag = -1
-        return x, error, flag
+    # Check if spectral radius of E - D^-1 * A < 1 to guarantee convergence
+    if np.max(np.abs(np.linalg.eigvals(np.eye(20) - np.dot(np.linalg.inv(D), A)))) < 1:
+        flag = 2
 
     # Check if A is sharply diagonally dominant to guarantee convergence
     if np.all(np.abs(np.diag(A)) > np.sum(np.abs(A), axis=1) - np.abs(np.diag(A))):
         flag = 2
 
     while error > tol and k < maxiter:
+        # Iterative refinement
+        if ref:
+            r = np.dot(A, x) - b
+            y = np.dot(np.linalg.inv(A), r)
+            x = x - y
+
         # D^-1 * (b - (L + U) * x)
         x = np.dot(np.linalg.inv(D), b - np.dot(L + U, x))
         # ||(A * x - b)|| / ||(b)|| - where ||.|| is the Euclidean norm 
@@ -33,10 +38,10 @@ def jacobi(A, b, tol, maxiter):
     if error <= tol:
         flag = 1
 
-    return x, error, flag
+    return x, k, error, flag
 
 # Function that solves a system of linear equations using the matrix method with Gauss-Seidel's method
-def gauss_seidel(A, b, tol, maxiter):
+def gauss_seidel(A, b, tol, maxiter, ref):
     k = 0
     D = np.diag(np.diag(A))
     L = np.tril(A) - D
@@ -45,16 +50,21 @@ def gauss_seidel(A, b, tol, maxiter):
     error = np.linalg.norm(np.dot(A, x) - b)
     flag = 0
 
-    # Check if spectral radius of E - (D + L)^-1 * A >= 1 to guarantee non-convergence
-    if np.max(np.abs(np.linalg.eigvals(np.eye(20) - np.dot(np.linalg.inv(D + L), A)))) >= 1:
-        flag = -1
-        return x, error, flag
+    # Check if spectral radius of E - (D + L)^-1 * A < 1 to guarantee convergence
+    if np.max(np.abs(np.linalg.eigvals(np.eye(20) - np.dot(np.linalg.inv(D + L), A)))) < 1:
+        flag = 2
 
     # Check if A is symmetric and positive definite and has positive diagonal to guarantee convergence
     if np.allclose(A, A.T) and np.all(np.linalg.eigvals(A) > 0) and np.all(np.diag(A) > 0):
         flag = 2
 
     while error > tol and k < maxiter:
+        # Iterative refinement
+        if ref:
+            r = np.dot(A, x) - b
+            y = np.dot(np.linalg.inv(A), r)
+            x = x - y
+
         # (L + D)^-1 * (b - U * x)
         x = np.dot(np.linalg.inv(L + D), b - np.dot(U, x))
         # ||(A * x - b)|| / ||(b)|| - where ||.|| is the Euclidean norm 
@@ -64,7 +74,7 @@ def gauss_seidel(A, b, tol, maxiter):
     if error <= tol:
         flag = 1
 
-    return x, error, flag   
+    return x, k, error, flag   
 
 # Main program
 if __name__ == '__main__':
@@ -73,6 +83,7 @@ if __name__ == '__main__':
                     help = 'Parameter which is used in construction of matrix A and vector b')
     arg.add_argument('-m', '--method', type = str, default='jacobi', choices=['jacobi', 'gauss_seidel'], 
                     help = 'Method to solve the system of linear equations')
+    arg.add_argument('-r', '--refinement', type = bool, default=False, help='Use iterative refinement')
     args = vars(arg.parse_args())
 
     gamma = args['gamma']
@@ -98,18 +109,19 @@ if __name__ == '__main__':
     
     tol = 1e-5
     maxiter = 1000
+    ref = args['refinement']
 
     if args['method'] == 'jacobi':
-        x, error, flag = jacobi(A, b, tol, maxiter)
+        x, k, error, flag = jacobi(A, b, tol, maxiter, ref)
     elif args['method'] == 'gauss_seidel':
-        x, error, flag = gauss_seidel(A, b, tol, maxiter)
+        x, k, error, flag = gauss_seidel(A, b, tol, maxiter, ref)
     else:
         print('The method is not valid')
 
     if flag == -1:
         print('The method did not converge due to non-convergence')
     elif flag == 1:
-        print(f'The solution is: {x}\nThe error is: {error}')
+        print(f'The solution is: {x}\nThe error is: {error}\nThe number of iterations is: {k}')
     elif flag == 2:
         print('The method did not converge due to precision issues, but it is theoretically convergent')
     else:
